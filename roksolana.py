4bcbabf4d75a3e46885f172c9ba46663c2b6f04c
Alessandro <gort818@gmail.com>
2016-06-10 20:20:35 -0700

    Add files via upload

--- roksolana.py
+++ roksolana.py
@@ -17,6 +17,8 @@ class Browser:
     self.refresh = self.builder.get_object("refresh")
     self.stop = self.builder.get_object("stop")
     self.url = self.builder.get_object("url")
+    self.spinner = self.builder.get_object("spinner")
+    self.progressbar = self. builder.get_object("progressbar")
     self.window = self.builder.get_object("window1")
     self.window.connect('destroy', lambda w: Gtk.main_quit())
     self.scrolledwindow = self.builder.get_object("scrolledwindow")
@@ -27,7 +29,13 @@ class Browser:
     self.webview.open('http://google.com')
     self.webview.connect('title-changed', self.change_title)
     self.webview.connect('load-committed', self.change_url)
+    self.webview.connect('load-committed', self.spinner_on)
+    self.webview.connect('load_finished',self.spinner_off)
+    #self.webview.connect('load-committed', self.progress_on)
+    #self.webview.connect('load-progress-changed', self.progress_change)
+    #self.webview.connect('document_load_finished',self.progress_off)
     self.webview.show()	
+   
 
   
   def on_url_activate(self, widget):
@@ -55,7 +63,23 @@ class Browser:
   def change_url(self, widget, frame):
      uri = frame.get_uri()
      self.url.set_text(uri)
-  
+     self.back.set_sensitive(self.webview.can_go_back() )
+     self.forward.set_sensitive(self.webview.can_go_forward() )
+     
+     
+	
+  def spinner_on(self,widget,frame):
+     self.spinner.start()
+  def spinner_off(self, widget,frame):
+     self.spinner.stop()
+     
+  #def progress_on(self,widget,frame):
+     #self.progressbar.pulse()
+     
+  #def progress_change(self,wiget,frame):
+   #  self.progressbar.set_pulse_step(.5)
+  #def progress_off(self,widget,frame):
+     #self.progressbar.set_pulse_step(0)
   
 
 def main ():
@@ -64,3 +88,4 @@ def main ():
 
 if __name__ == "__main__":
   main()
+
